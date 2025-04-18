5dd1d44e8e20064244a13245e862a9937ccbdcea
dhartisolanki-chpl <dharti.solanki@chplgroup.org>
2025-04-18 18:37:10 +0530

    dom project

--- javascript_tuitorial/functions/04_arrow.js
+++ javascript_tuitorial/functions/04_arrow.js
@@ -9,10 +9,10 @@ const user = {
         // console.log(this)
     }
 }
-user.welcomeMessage()
+// user.welcomeMessage()
 
 // user.username= "swati"
-// user.welomeMessage()
+// user.welcomeMessage()
 
 // console.log(this)
 
@@ -29,7 +29,39 @@ user.welcomeMessage()
 // }    // undefined
 
 // const chai = () => {
-//     let username = "dharti"
-//     console.log(this.username)
+//     // let username = "dharti"
+//     // console.log(this.username)
+
+//     // console.log(this)
 // }
 // chai()
+ 
+//arrow functiom
+
+//explisite return
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+//implisite return
+// const addTwo = (num1, num2) => num1 + num2
+
+//second
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+// console.log(addTwo(3,4))
+
+
+//object return
+
+// const addTwo = (num1, num2) => ({username: "Dharti"})
+
+// console.log(addTwo(3,4))
+
+
+const myArray = [4,5,6,7,8]
+
+myArray.forEach( function () {})
+myArray.forEach( () => {})
